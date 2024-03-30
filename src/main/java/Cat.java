cf63d5eda020ca4813a144a81cfca1b9c6332bef
Mr.Robert <97745785+RobertKaribov@users.noreply.github.com>
2024-03-30 19:20:45 +0300

    1 commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,18 @@
+public class Cat {
+    private String name;
+
+    // Конструктор
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    // Геттер по имени
+    public String getName() {
+        return name;
+    }
+
+    // Сеттер для имени
+    public void setName(String name) {
+        this.name = name;
+    }
+}
