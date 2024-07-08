60dc909249a229154e7859938075852ddc74d5e1
trumpetman-r <jmxtrumpet@gmail.com>
2024-07-08 15:27:32 +0300

    2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat one = applicationContext.getBean(Cat.class);
+        Cat two = applicationContext.getBean(Cat.class);
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+        System.out.println(one.getName());
+        System.out.println(two.getName());
+        System.out.println(one == two);
     }
 }
